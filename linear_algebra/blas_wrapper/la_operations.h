aa223c2fe68f7bd27aca81e18cb6659e6b677bae
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2023-04-24 18:25:08 +0200

    auto indent

--- linear_algebra/blas_wrapper/la_operations.h
+++ linear_algebra/blas_wrapper/la_operations.h
@@ -7,27 +7,26 @@
 namespace la_operations
 {
 
-template <typename T>
-void copy_data(const la_objects::LABaseObject<T>& _src, la_objects::LABaseObject<T>& _dest)
-{
-    _dest.resize(_src.n_rows(), _src.n_cols());
-    blas_wrapper::copy(_src.n_rows() * _src.n_cols(),
-                       _src.get_data_ptr(),
-                       1,
-                       _dest.get_data_ptr(),
-                       1);
-}
+    template <typename T>
+    void copy_data(const la_objects::LABaseObject<T> &_src, la_objects::LABaseObject<T> &_dest)
+    {
+        _dest.resize(_src.n_rows(), _src.n_cols());
+        blas_wrapper::copy(_src.n_rows() * _src.n_cols(),
+                           _src.get_data_ptr(),
+                           1,
+                           _dest.get_data_ptr(),
+                           1);
+    }
 
-template <typename T>
-void scale(const T& _scale, la_objects::LABaseObject<T>& _dest)
-{
-    int elem_dist = 1;
-    int n = _dest.n_rows() * _dest.n_cols();
+    template <typename T>
+    void scale(const T &_scale, la_objects::LABaseObject<T> &_dest)
+    {
+        int elem_dist = 1;
+        int n = _dest.n_rows() * _dest.n_cols();
 
-    blas_wrapper::scal(n, _scale, _dest.get_data_ptr(), elem_dist);
-}
+        blas_wrapper::scal(n, _scale, _dest.get_data_ptr(), elem_dist);
+    }
 
 } // END NAMESPACE la_operations
 
 #endif // LA_OPERATIONS
-
