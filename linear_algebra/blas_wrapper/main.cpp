beb402b6c5a16b818fb373856f3e9843234c554a
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2023-04-25 16:35:40 +0200

    time the operation

--- linear_algebra/blas_wrapper/main.cpp
+++ linear_algebra/blas_wrapper/main.cpp
@@ -1,5 +1,6 @@
 #include <iomanip>
 #include <la_operations.h>
+#include <chrono>
 
 template <typename T>
 void initialize_matrix(la_objects::LABaseObject<T> &A) 
@@ -20,7 +21,7 @@ int main(int argc, char * argv[])
     if (argc != 2) { std::cout << "Please input the matrix size"; return 1; }
 
     int m = atoi(argv[1]);
-    std::cout << m << std::endl;
+    // std::cout << m << std::endl;
 
     if (m <= 0) { std::cout << "Please enter a valid matrix size > 0"; return 1; }
 
@@ -28,6 +29,14 @@ int main(int argc, char * argv[])
     
     initialize_matrix(A);
 
+    // Ref: https://www.geeksforgeeks.org/measure-execution-time-function-cpp/
+    auto start = std::chrono::high_resolution_clock::now();
+    la_operations::copy_data(A, B);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    
+    std::cout << duration.count() << std::endl;
 
     // std::cout << std::setprecision(20) << std::scientific;
     // std::cout << "A:" << std::endl; A.print(std::cout); std::cout << std::endl;
