87c4a579e43e07b5c095e338332f8446a2ef38a2
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2023-04-25 20:15:32 +0200

    main runs

--- linear_algebra/gemm_wrapper/la_operations.h
+++ linear_algebra/gemm_wrapper/la_operations.h
@@ -33,7 +33,24 @@ void contract(const la_objects::LAMatrix<T>& _larg, const la_objects::LAMatrix<T
 {
     _dest.resize(_larg.n_rows(), _rarg.n_cols());
 
-    blas_wrapper::gemm();
+    // gemm(char transa, char transb, int m, int n, int k, float alpha,
+    //              const float * const A, int lda, const float * const B, int ldb,
+    //              float beta, float * C, int ldc)
+
+    if (_larg.n_cols() != _rarg.n_rows())
+    {
+        throw std::runtime_error("Left cols != Right rows, multiplication not possible.");
+    }
+    
+    // https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm
+    blas_wrapper::gemm('C', 'C', 
+        _dest.n_rows(), _dest.n_cols(), _larg.n_rows(),
+        1, 
+        _larg.get_data_ptr(), _larg.leading_dim(),
+        _rarg.get_data_ptr(), _rarg.leading_dim(),
+        1,
+        _dest.get_data_ptr(), _dest.leading_dim()
+    );
 }
 
 } // END NAMESPACE la_operations
