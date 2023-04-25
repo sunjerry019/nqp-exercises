1d52ff3fa98caccf5a1aebbf9ba32bb2dee5674c
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2023-04-25 17:38:31 +0200

    first slurm attempt

--- linear_algebra/blas_wrapper/scaling.py
+++ linear_algebra/blas_wrapper/scaling.py
@@ -1,7 +1,7 @@
 #/usr/bin/env python3
 
 # 50 cores
-# We distribute to 50 cores, on each one we run the main program with the size 20 times, obtaining 1000 data points
+# We distribute to 20 cores, on each one we run the main program with the size 20 times, obtaining 800 data points
 # Then we get an average over all the results
 # Repeat until m = 5000
 
@@ -16,35 +16,47 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 start = 1
-stop  = 10
-repeats = 2
+stop  = 5000
+repeats = 20
 
-arr = np.linspace(start = start, stop = stop, num = (stop - start + 1), endpoint = True, dtype = int)
-timings = np.zeros(shape = (len(arr), repeats))
+numpts = (stop - start + 1)
 
-shape = timings.shape
-for midx in range(shape[0]):
-    for iteration in range(shape[1]):
-        m = arr[midx]
-        timings[midx, iteration] = int(subprocess.check_output(["./main", f"{m}"]).strip())
+arr = np.linspace(start = start, stop = stop, num = numpts, endpoint = True, dtype = int)
+timings = np.zeros(shape = (repeats, numpts))
+
+for midx in range(numpts):
+    m = arr[midx]
+
+    print(rank, m)
+    
+    for iteration in range(repeats):
+        timings[iteration, midx] = int(subprocess.check_output(["./main", f"{m}"]).strip())
 
 recvBuffer = None
 if rank == 0:
-    recvBuffer = np.empty([size, shape[0], shape[1]])
+    recvBuffer = np.empty([size, repeats, numpts])
 
 comm.Gather(timings, recvBuffer, root = 0)
 
 if rank == 0: 
-    print(recvBuffer.shape)
-    print(recvBuffer)
+    # print(recvBuffer.shape)
+    collapsed = recvBuffer.reshape((repeats * size, numpts)).T
+    
+    # print(collapsed.size)
+    # print(collapsed)
 
-    avg = np.average(recvBuffer, axis = (0,2))
+    avg    = np.average(collapsed, axis = 1)
+    stddev = np.std(collapsed, axis = 1)
 
-    # find stddev
+    # print(avg.shape)
+    # print(avg)
+
+    # print(stddev.shape)
+    # print(stddev)
 
     now = datetime.now()
     dt_string = now.strftime("%Y%m%d-%H:%M:%S")
-    with open(f"scaling_result_{dt_string}.tsv", "w") as f:
-        f.write("# m\tRuntime\tstddev\n")
-        for midx in range(shape[0]):
-            f.write("")
+    with open(f"slurm/scaling_result_{dt_string}.tsv", "w") as f:
+        f.write("# m\tRuntime(us)\tstddev(us)\n")
+        for midx in range(numpts):
+            f.write(f"{arr[midx]}\t{avg[midx]}\t{stddev[midx]}\n")
