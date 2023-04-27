4eed578c0f32a45c4b016eb93e4e89db93eb023e
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2023-04-27 16:42:10 +0200

    Problem 2.b main outputs timings of both methods, with optional flag to disable naive

--- linear_algebra/gemm_wrapper/main.cpp
+++ linear_algebra/gemm_wrapper/main.cpp
@@ -3,32 +3,75 @@
 #include <la_operations.h>
 #include <la_objects.cpp>
 
-int main(int /*argc*/, char **/*argv[]*/)
+#include <chrono>
+
+template <typename T>
+void initialize_matrix(la_objects::LABaseObject<T> &A, int NMAX) 
 {
-    la_objects::LAMatrix<double> A(2, 3);
-    la_objects::LAMatrix<double> B(3, 2);
-
-    la_objects::LAMatrix<double> C;
-
     for (unsigned int r = 0; r < A.n_rows(); r++)
     {
         for (unsigned int c = 0; c < A.n_cols(); c++)
         {
-            A(r, c) = c + r*A.leading_dim();
+            A(r, c) = rand() % NMAX;
         }
     }
 
-    for (unsigned int r = 0; r < B.n_rows(); r++)
+    return;
+}
+
+int main(int argc, char * argv[])
+{
+    if (argc != 2 && argc != 3) { std::cout << "Please input the matrix size, with optional flag for only using xgemm; e.g. \n\tmain 10 1\nfor using xgemm only"; return 1; }
+
+    int m = atoi(argv[1]);
+    int xgemmOnly = 0;
+    
+    if (argc == 3) xgemmOnly = atoi(argv[2]);
+
+    if (m <= 0) { std::cout << "Please enter a valid matrix size > 0"; return 1; }
+
+    la_objects::LAMatrix<double> A(m, m), B(m, m);
+    la_objects::LAMatrix<double> C_gemm(m, m), C_naive(m, m);
+
+    std::srand((unsigned) std::time(NULL));
+
+    // We initialize the matrices
+    initialize_matrix(A, m);
+    initialize_matrix(B, m);
+
+    // xgemm
+    auto gemm_start = std::chrono::high_resolution_clock::now();
+    C_gemm = A * B;
+    auto gemm_end = std::chrono::high_resolution_clock::now();
+    auto gemm_duration = std::chrono::duration_cast<std::chrono::microseconds>(gemm_end - gemm_start);
+
+    int64_t naive_microsecond = 0;
+    if (!xgemmOnly)
     {
-        for (unsigned int c = 0; c < B.n_cols(); c++)
+        // Naive Way
+        int answer = 0;
+        auto naive_start = std::chrono::high_resolution_clock::now();
+        for (unsigned int r = 0; r < C_naive.n_rows(); r++)
         {
-            B(r, c) = c + r*B.leading_dim();
+            for (unsigned int c = 0; c < C_naive.n_cols(); c++)
+            {
+                for (unsigned int k = 0; k < A.n_cols(); k++)
+                {
+                    answer += A(r, k) * B(k, c);
+                }
+                C_naive(r, c) = answer;
+                answer = 0;
+            }
         }
+        auto naive_end = std::chrono::high_resolution_clock::now();
+        auto naive_duration = std::chrono::duration_cast<std::chrono::microseconds>(naive_end - naive_start);
+        naive_microsecond = naive_duration.count();
     }
 
-    C = A * B;
-
-    std::cout << "A*B:" << std::endl << C << std::endl;
+    // std::cout << "C_gemm:" << std::endl << C_gemm << std::endl;
+    std::cout << gemm_duration.count() << std::endl;
+    // std::cout << "C_naive:" << std::endl << C_naive << std::endl;
+    if (!xgemmOnly) std::cout << naive_microsecond << std::endl;
 
     return 0;
 }
