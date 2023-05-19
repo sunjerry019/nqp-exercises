02394fb5326455d8d32cd164b2388da04cadd331
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2023-05-20 01:15:49 +0200

    (2.d)

--- exact_diagonalization/2_ground_state.jl
+++ exact_diagonalization/2_ground_state.jl
@@ -36,9 +36,30 @@ using Test
     end
 end
 
-_h_array = collect(range(0, 100, length=100))
-L = 3
+# _h_array = collect(range(0, 100, length=100))
+# L = 3
 
-for h_idx in eachindex(_h_array)
-    println(ground_state_energy_density(L, _h_array[h_idx]))
+# for h_idx in eachindex(_h_array)
+#     println(ground_state_energy_density(L, _h_array[h_idx]))
+# end
+
+# (2.d)
+function approxequal(A :: TransverseFieldIsing.QuantumMechanics.State, B :: State) :: Bool
+    return (A.L == B.L) ? (isapprox(A.state, B.state, rtol = 0.0000001)) : false
 end
+
+@testset "Limit h → 0" begin
+    L = 4
+    gs_0 = ground_state(L, 0)
+
+    @test approxequal(gs_0, GetFerromagneticStateZ(L, true)) || approxequal(gs_0, GetFerromagneticStateZ(L, false))
+end
+
+@testset "Limit h → ∞" begin
+    L = 2
+    gs_0 = -1 * ground_state(L, 10000000)
+
+    @test approxequal(gs_0, GetFerromagneticStateX(L, true)) || approxequal(gs_0, GetFerromagneticStateX(L, false))
+end
+
+
