8d869bc21948a1e71a136a801bc5f62404bb882d
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2023-05-19 14:30:30 +0200

    add get ferromagnetic X

--- exact_diagonalization/1_tensor_product.jl
+++ exact_diagonalization/1_tensor_product.jl
@@ -13,12 +13,26 @@ using LinearAlgebra
         x = GetRandomState(L)
         @test x.L == L
         @test size(x.state) == (2^L,)
-        @test norm(x.state) == 1
+        @test norm(x.state) ≈ 1
     end
     @testset "Ferromagnetic Z" begin
         x = GetFerromagneticStateZ(1)
         @test x.L == 1
         @test x.state == [0.0+0.0im, 1.0+0.0im] || x.state == [1.0+0.0im, 0.0+0.0im] 
         @test size(x.state) == (2,)
+        @test norm(x.state) ≈ 1
+    end
+    @testset "Ferromagnetic XX" begin
+        x = GetFerromagneticStateX(2)
+        twox = 2*x
+
+        xplus  = ones(Complex{Float64}, 4)
+        xminus = ones(Complex{Float64}, 4)
+        xminus[2] = -1
+        xminus[3] = -1
+
+        @test x.L == 2
+        @test norm(x.state) ≈ 1
+        @test twox.state == xplus || twox.state == xminus 
     end
 end
--- exact_diagonalization/qm.jl
+++ exact_diagonalization/qm.jl
@@ -4,7 +4,9 @@ module QuantumMechanics
     using Random
     using LinearAlgebra
 
-    export GetRandomState, GetFerromagneticStateZ
+    import Base: *
+
+    export GetRandomState, GetFerromagneticStateZ, GetFerromagneticStateX, *
 
     abstract type HilbertSpace end
 
@@ -13,6 +15,11 @@ module QuantumMechanics
         state :: Vector{Complex{Float64}}  
         # The actual state vector, quantization axis = z
         # We use the tensor product basis
+    end
+
+    function *(A :: Number, B :: State) 
+        C = State(B.L, A * B.state)
+        return C
     end
 
     function GetRandomState(L :: Integer) :: State
@@ -32,5 +39,22 @@ module QuantumMechanics
         vec = zeros(Complex{Float64}, 2^L)
         vec[rand(Bool) ? 1 : end] = 1
         return State(L, vec)
+    end
+
+    """
+    Returns a random Ferromagnetic state along x
+
+    We use the kronecker (tensor) product this time.
+    """
+    function GetFerromagneticStateX(L :: Integer) :: State
+        # Create a non-normalized eigenstate first
+        vec = ones(Complex{Float64}, 2)
+        vec[2] = rand(Bool) ? 1 : -1
+
+        # Tensor product it L times
+        args = ntuple(x -> vec, L)
+        fullvec = kron(args...)
+
+        return State(L, fullvec/norm(fullvec))
     end
 end
