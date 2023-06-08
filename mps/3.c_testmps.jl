29b5cfddb17d1bdb2948f2643759948e2e0afca5
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2023-06-08 23:31:22 +0200

    rearrange usings

--- mps/3.c_testmps.jl
+++ mps/3.c_testmps.jl
@@ -5,11 +5,13 @@ include("testsetskip.jl")
 push!(LOAD_PATH, "./")
 push!(LOAD_PATH, "./exact_diagonalization/")
 
-using MatrixProductStates
 using QM
+using TransverseFieldIsing
+using MatrixProductStates
 using Test
 using LinearAlgebra
 
 S = QM.GetFerromagneticStateX(7)
 Q = create_MPS_from_State(S)
 display(Q)
+
