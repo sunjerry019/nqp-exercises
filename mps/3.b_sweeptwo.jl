cb303641e3a055c01a587db47c30b7610cef5597
Yudong Sun <Yudong.Sun@physik.uni-muenchen.de>
2023-06-08 17:06:36 +0200

    RTL seems to be done, using the convention i figured out in accumulator

--- mps/3.b_sweeptwo.jl
+++ mps/3.b_sweeptwo.jl
@@ -28,11 +28,34 @@ function sweep_two_states_LTR(ϕ :: MPS, ϕ_tilde :: MPS) :: Tuple{MPS, MPS, Dic
     return ϕ, ϕ_tilde, omega_L
 end
 
+function sweep_two_states_RTL(ϕ :: MPS, ϕ_tilde :: MPS) :: Tuple{MPS, MPS, Dict{Integer, Union{Array, UniformScaling}}}
+    if ϕ.L != ϕ_tilde.L
+        throw(DimensionMismatch("The two states must have the same num of sites."))
+    end
+
+    ϕ       = construct_MPS(deepcopy(ϕ.tensor_sets))
+    ϕ_tilde = construct_MPS(deepcopy(ϕ_tilde.tensor_sets))
+
+    omega_R = Dict{Integer, Union{Array, UniformScaling}}()
+    omega_R[ϕ.L] = I # 0 is -1 in the question
+
+    # Sweep from 1 to (L-1)
+    for j in reverse(2:(ϕ.L-1))
+        make_orthogonal_right!(ϕ, j)
+        make_orthogonal_right!(ϕ_tilde, j)
+
+        _left = split_left(fuse_left(ϕ.tensor_sets[j]) * omega_R[j+1], ϕ.ds[j])
+        omega_R[j] = fuse_right(_left) * fuse_right(ϕ_tilde.tensor_sets[j])'
+    end
+
+    return ϕ, ϕ_tilde, omega_R
+end
+
 ψ       = create_random_state(5,2)
 ψ_tilde = create_random_state(5,2)
-x, x_tilde, omega_L = sweep_two_states_LTR(ψ, ψ_tilde)
+x, x_tilde, omega_R = sweep_two_states_RTL(ψ, ψ_tilde)
 
-display(omega_L)
+display(omega_R)
 
 # Prepare a state with gauge center at j = 0
 # run sweep_two_states_LTR
